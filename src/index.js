8a7bd30bcf22949d78a02b8c4d8d0a3543d4d604
yertxt <anrra2000@gmail.com>
2023-10-22 17:33:57 -0500

    header partial

---
+++ src/index.js
@@ -0,0 +1,34 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen = mobileMenu.classList.contains('is-open');
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = isMenuOpen
+      ? 'enableBodyScroll'
+      : 'disableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+
+    // Oculta o muestra el botón correspondiente según el estado del menú
+    openMenuBtn.style.display = isMenuOpen ? 'block' : 'none';
+    closeMenuBtn.style.display = isMenuOpen ? 'none' : 'block';
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+
+    // Restaura la visibilidad de los botones en pantallas más grandes
+    openMenuBtn.style.display = 'block';
+    closeMenuBtn.style.display = 'none';
+  });
+})();
