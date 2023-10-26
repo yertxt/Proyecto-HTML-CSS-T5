9cdc054e59ecec2b8ab9239558f497e7ffbe44a8
yertxt <anrra2000@gmail.com>
2023-10-26 18:42:57 -0500

    header update

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+// (() => {
+//   const mobileMenu = document.querySelector('.js-menu-container');
+//   const openMenuBtn = document.querySelector('.js-open-menu');
+//   const closeMenuBtn = document.querySelector('.js-close-menu');
 
-  const toggleMenu = () => {
-    const isMenuOpen = mobileMenu.classList.contains('is-open');
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+//   const toggleMenu = () => {
+//     const isMenuOpen = mobileMenu.classList.contains('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = isMenuOpen
-      ? 'enableBodyScroll'
-      : 'disableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+//     const scrollLockMethod = isMenuOpen
+//       ? 'enableBodyScroll'
+//       : 'disableBodyScroll';
+//     bodyScrollLock[scrollLockMethod](document.body);
 
-    // Oculta o muestra el botón correspondiente según el estado del menú
-    openMenuBtn.style.display = isMenuOpen ? 'block' : 'none';
-    closeMenuBtn.style.display = isMenuOpen ? 'none' : 'block';
-  };
+//     // Oculta o muestra el botón correspondiente según el estado del menú
+//     openMenuBtn.style.display = isMenuOpen ? 'block' : 'none';
+//     closeMenuBtn.style.display = isMenuOpen ? 'none' : 'block';
+//   };
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+//   openMenuBtn.addEventListener('click', toggleMenu);
+//   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//     bodyScrollLock.enableBodyScroll(document.body);
 
-    // Restaura la visibilidad de los botones en pantallas más grandes
-    openMenuBtn.style.display = 'block';
-    closeMenuBtn.style.display = 'none';
-  });
-})();
+//     // Restaura la visibilidad de los botones en pantallas más grandes
+//     openMenuBtn.style.display = 'block';
+//     closeMenuBtn.style.display = 'none';
+//   });
+// })();
 
 // (() => {
 //   const mobileMenu = document.querySelector('.js-menu-container');
@@ -60,3 +60,32 @@
 //     bodyScrollLock.enableBodyScroll(document.body);
 //   });
 // })();
+
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
