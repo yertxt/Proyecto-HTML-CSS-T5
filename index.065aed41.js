3854bbb8b3e0640ae4c44108532b40f163cc54e3
yertxt <66653119+yertxt@users.noreply.github.com>
2023-10-27 00:01:15 +0000

    Deploying to gh-pages from @ yertxt/Proyecto-HTML-CSS-T5@21ab23c9fa9df60542ce75c0df50a5c957c6ea62 🚀

---
+++ index.065aed41.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[popup2-open]"),closeModalBtn:document.querySelector("[popup2-close]"),modal:document.querySelector("[data-modal-popup2]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.065aed41.js.map
